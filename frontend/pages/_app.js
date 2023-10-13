4b0e010a8ea3a0ea625494514b6e8590a4bd8a63
javixlive <javixlive@hotmail.com>
2023-10-13 13:44:23 -0600

    NProgress

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,17 @@
+import NProgress from "nprogress";
 import Page from "../components/Page";
+import Router from "next/router";
+import 'nprogress/nprogress.css'
+
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps}) {
-    return <Page>
+    return (
+    <Page>
         <Component { ...pageProps } />
     </Page>
+    );
 }
--- frontend/pages/account.js
+++ frontend/pages/account.js
@@ -1,5 +1,9 @@
 const AccountPage = () => {
-	return <div><h1>Account</h1></div>;
+	return (
+	<div>
+		<h1>Account</h1>
+	</div>
+	);
 };
 
 export default AccountPage;
